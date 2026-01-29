47d9fd130b71220e5f74460e8b33e09aa5237c71
Gilles Kinet <gilleskinet@gmail.com>
2026-01-29 15:28:49 +0100

    feat: create method to reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let array = [];
+  for (let i = 0; i < str.length; i++) {
+    array.push(str[i]);
+  }
+  return array.reverse().join("");
 };
 
 // Do not edit below this line
