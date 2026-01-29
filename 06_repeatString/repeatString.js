7803987fe420dfca3544bd6d4953e5c4cab3f719
Gilles Kinet <gilleskinet@gmail.com>
2026-01-29 14:52:33 +0100

    feat: create code to repeat string x amount of times + throw error for negative numbers

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, number) {
+  if (number >= 0) {
+    let arr = [];
+    for (let i = 0; i < number; i++) {
+      arr.push(str);
+    }
+    return arr.join("");
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
