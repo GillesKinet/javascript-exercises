5ef3acacaf81ba90c66ec645b8dd236a1ea7cef8
Gilles Kinet <gilleskinet@gmail.com>
2026-01-30 10:05:16 +0100

    feat: create summ all using for loop

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (typeof a === "string" || typeof b === "string") {
+    return "ERROR";
+  } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  } else if (a < 0 || b < 0) {
+    return "ERROR";
+  } else {
+    let c = a < b ? a : b;
+    const d = a > b ? a : b;
+    let sum = 0;
+    for (let i = c; i < d + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
