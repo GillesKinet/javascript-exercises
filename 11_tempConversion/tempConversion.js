d94f30aed9373807d13002f03529e79a07640768
Gilles Kinet <gilleskinet@gmail.com>
2026-01-30 10:42:49 +0100

    feat: create method to convert temp from fahrenheit to celsius and vice versa

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+function roundNumber(number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function (temp) {
+  let tempConverted = (temp - 32) * (5 / 9);
+  return roundNumber(tempConverted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let tempConverted = temp * (9 / 5) + 32;
+  return roundNumber(tempConverted);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
