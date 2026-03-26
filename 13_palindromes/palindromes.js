8e2500d9d672dda301f414f74bfb283b28d2e27f
Gilles Kinet <gilleskinet@gmail.com>
2026-03-26 13:10:04 +0100

    feat: finish palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+function isAlphaNumeric(str) {
+  return /^[a-zA-Z0-9]+$/.test(str);
+}
 
+const palindromes = function (string) {
+  const cleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((item) => isAlphaNumeric(item))
+    .join();
+
+  const reversedString = cleanString.split("").reverse().join("");
+
+  return cleanString === reversedString;
 };
 
 // Do not edit below this line
