9921ad602db1d49cabe5b9ad94cb56794e4bf495
Gilles Kinet <gilleskinet@gmail.com>
2026-03-29 20:15:38 +0200

    feat: finish fibo

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  const numberInt = parseInt(number);
+  if (numberInt < 0) {
+    return "OOPS";
+  }
+  let fibo = 0;
 
+  if (numberInt === 0) {
+    return fibo;
+  }
+
+  if (numberInt === 1) {
+    return ++fibo;
+  }
+
+  let firstVal = 0;
+  let secondVal = 1;
+
+  for (let i = 2; i <= numberInt; i++) {
+    fibo = firstVal + secondVal;
+    firstVal = secondVal;
+    secondVal = fibo;
+  }
+
+  return fibo;
 };
 
 // Do not edit below this line
