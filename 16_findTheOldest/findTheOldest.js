22f421ded0c7eca7e7712657f8b556f42bba5bdc
Gilles Kinet <gilleskinet@gmail.com>
2026-03-30 10:37:51 +0200

    feat: finish find the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+function calcAge(yearOfDeath, yearOfBirth) {
+  let recalcYearOfDeath = yearOfDeath;
+  if (recalcYearOfDeath === undefined) {
+    recalcYearOfDeath = new Date().getFullYear();
+  }
+  return recalcYearOfDeath - yearOfBirth;
+}
+const findTheOldest = function (arr) {
+  const ageArray = arr.map((item) =>
+    calcAge(item.yearOfDeath, item.yearOfBirth),
+  );
+  const max = Math.max(...ageArray);
+  console.log(max);
+  const oldestPerson = arr.find((item) => {
+    return calcAge(item.yearOfDeath, item.yearOfBirth) === max;
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
